938e3896a366d5f890eb81fd229d6a31d1d0e6e8
mohammed_khan <mohammed.khan@turbonomic.com>
2026-05-30 12:57:41 -0400

    changes

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-east-2"  # Set your desired AWS region
+}
+
+resource "aws_instance" "demo_instance" {
+    ami           = "ami-00e801948462f718a"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+}
