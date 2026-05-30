834c964f70091fcb5bb92c36eca7453668c7eb94
mohammed_khan <mohammed.khan@turbonomic.com>
2026-05-30 15:19:29 -0400

    updated main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 
+
 provider "aws" {
-    region = "us-east-2"  # Set your desired AWS region
+    region = "us-east-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "demo_instance" {
